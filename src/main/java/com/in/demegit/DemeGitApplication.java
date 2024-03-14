2fe81e116233fd7d2a44347e495dccd873ea9783
mdjav <mdjavadeveloper@gmail.com>
2024-03-14 20:16:27 +0530

    i added for loop and printing 0 to 9.

--- src/main/java/com/in/demegit/DemeGitApplication.java
+++ src/main/java/com/in/demegit/DemeGitApplication.java
@@ -9,6 +9,12 @@ public class DemeGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemeGitApplication.class, args);
 		System.out.println("this is testing purpose....");
+
+		for (int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
+
 	}
 
 }
