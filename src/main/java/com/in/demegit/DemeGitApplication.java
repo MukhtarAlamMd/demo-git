efd83eb2a7172bc81df8441cb93d52add91c4f3f
mdjav <mdjavadeveloper@gmail.com>
2024-03-14 20:10:28 +0530

    i am committing demo-git

---
+++ src/main/java/com/in/demegit/DemeGitApplication.java
@@ -0,0 +1,14 @@
+package com.in.demegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemeGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemeGitApplication.class, args);
+		System.out.println("this is testing purpose....");
+	}
+
+}
---
+++ src/test/java/com/in/demegit/DemeGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.demegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
